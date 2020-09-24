66abe1189b200479eae241fdf94fa1ade719822b
ajdagod <67406670+ajdagod@users.noreply.github.com>
2020-09-24 19:39:46 +0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,33 @@
+const World=Matter.World;
+const Engine=Matter.Engine;
+const Bodies=Matter.Bodies;
+var engine,world;
+var backimg;
+
+function preload(){
+backimg=loadImage("background.jpeg");
+
+
+}
+
+function setup(){
+engine=Engine.create();
+world=engine.world;
+canvas=createCanvas(700,450);
+enemy1=new enemy(200,200);
+person1=new person(200,200,200,200);
+gun1=new gun(100,100,10,10);
+}
+
+function draw(){    
+background(backimg);
+
+person1.display();
+
+}
+
+
+
+
+
+
